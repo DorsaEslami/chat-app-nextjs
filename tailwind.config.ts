8f06132b1b86f500695ba928354ef7fa0b33e4b8
dorsa.eslami <dorsa.eslami97@gmail.com>
2025-03-29 18:19:54 +0330

    master:add clerk

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/providers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          default: "#2A2A5A"
+        }
+      },
+    },
+  },
+  plugins: [],
+
+};
+export default config;
